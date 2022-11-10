89e675e6e206e506d5eedb8a7d41747e202fbf1d
PeterLynchJ <35786843+PeterLynchJ@users.noreply.github.com>
2022-11-10 15:29:36 -0500

    Update PlayGround.java
    should trigger build

--- PlayGround.java
+++ PlayGround.java
@@ -2,5 +2,6 @@ public class PlayGround {
     public static void main(String[] args) {
         // what is this?
         System.out.println("Hello Jenkins");
+        System.out.println("trigger build on push");
     }
 }
